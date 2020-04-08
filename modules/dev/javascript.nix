7ad5bf282b5f8b86c2d12348b5306a8714fef43d
Matei Adriel <rafaeladriel11@gmail.com>
2020-04-08 21:15:37 +0300

    feat: including npm packages in nix-config!

--- modules/dev/javascript.nix
+++ modules/dev/javascript.nix
@@ -1,6 +1,9 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let fromNpm = import ./npm { inherit pkgs; };
+in {
   home-manager.users.adrielus.home.packages = with pkgs; [
     nodejs
     nodePackages.node2nix
+    fromNpm.pnpm
   ];
 }
