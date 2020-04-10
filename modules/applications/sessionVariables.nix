f59a21f69785decefba9d22210c2c16447204b97
Matei Adriel <rafaeladriel11@gmail.com>
2020-04-10 15:04:09 +0300

    fix: oops, even tho I deleted the variables I forgot to change the user after I copied the sessionVariables file from Bretts template lol

--- modules/applications/sessionVariables.nix
+++ modules/applications/sessionVariables.nix
@@ -1,6 +1,6 @@
 { ... }:
 with import ../../secrets.nix; {
-  home-manager.users.brett.home.sessionVariables = {
+  home-manager.users.adrielus.home.sessionVariables = {
     inherit GITHUB_TOKEN;
     GITHUB_USERNAME = "Mateiadrielrafael";
   };
