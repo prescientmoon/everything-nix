4c6ab670092754612c19824111c5e78eaed414f3
Matei Adriel <rafaeladriel11@gmail.com>
2021-11-27 20:54:35 +0200

    fix:changed version to 21.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-21.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
     nixos-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager.url = "github:nix-community/home-manager";
