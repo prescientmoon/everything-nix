40609495b8aab516ac16f5cb83fec52aeae5ae69
Matei Adriel <rafaeladriel11@gmail.com>
2021-11-27 20:07:26 +0200

    fix: removed rofi for now and readded easy-*-nix

--- flake.nix
+++ flake.nix
@@ -8,21 +8,23 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # easy-dhall-nix.url = "github:justinwoo/easy-dhall-nix";
-    # easy-dhall-nix.flake = false;
-    # # easy-dhall-nix.inputs.nixpkgs.follows = "nixpkgs";
+    easy-dhall-nix.url = "github:justinwoo/easy-dhall-nix";
+    easy-dhall-nix.flake = false;
 
-    # easy-purescript-nix.url = "github:justinwoo/easy-purescript-nix";
-    # easy-purescript-nix.flake = false;
-    # easy-purescript-nix.inputs.nixpkgs.follows = "nixpkgs";
+    easy-purescript-nix.url = "github:justinwoo/easy-purescript-nix";
+    easy-purescript-nix.flake = false;
   };
 
   outputs = { self, nixpkgs, home-manager, nixos-unstable,
-    # easy-purescript-nix,
-    # easy-dhall-nix,
+    easy-purescript-nix,
+    easy-dhall-nix,
     ... }: {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+        nixpkgs.overlays = [self: super: {
+          easy-purescript-nix = import easy-purescript-nix { inherit pgks };
+          easy-dhall-nix = import easy-dhall-nix { inherit pgks };
+        }];
         modules = [
           home-manager.nixosModules.home-manager
           ./hardware/laptop.nix
--- modules/applications/rofi.nix
+++ modules/applications/rofi.nix
@@ -3,9 +3,9 @@
     enable = true;
     font = "Source Code Pro 16";
     location = "center";
-    padding = 10;
-    lines = 7;
-    fullscreen = false;
+    # padding = 10;
+    # lines = 7;
+    # fullscreen = false;
     cycle = true;
     theme = "solarized_alternate";
   };
