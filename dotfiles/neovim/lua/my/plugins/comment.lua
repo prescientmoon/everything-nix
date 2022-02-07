611e8c30751e09b69813f63c08cdd8ee787ca247
Matei Adriel <rafaeladriel11@gmail.com>
2022-02-07 12:52:21 +0200

    feat: fixed .nix commentstring

--- dotfiles/neovim/lua/my/plugins/comment.lua
+++ dotfiles/neovim/lua/my/plugins/comment.lua
@@ -2,15 +2,17 @@ local M = {}
 
 -- Update comments for certain languages
 local function setCommentString(extension, commentString)
-    vim.cmd('autocmd BufEnter '.. extension .. ' :lua vim.api.nvim_buf_set_option(0, "commentstring", "' .. commentString .. '")')
-    vim.cmd('autocmd BufFilePost '.. extension .. ' :lua vim.api.nvim_buf_set_option(0, "commentstring", "' .. commentString ..'")')
+    vim.cmd('augroup set-commentstring-' .. extension)
+    vim.cmd('autocmd!')
+    vim.cmd('autocmd BufEnter *.' .. extension .. ' :lua vim.api.nvim_buf_set_option(0, "commentstring", "' .. commentString .. '")')
+    vim.cmd('autocmd BufFilePost *.' .. extension .. ' :lua vim.api.nvim_buf_set_option(0, "commentstring", "' .. commentString .. '")')
+    vim.cmd('augroup END')
 end
 
 function M.setup()
     require('nvim_comment').setup()
 
-    setCommentString(".nix", "# %s")
+    setCommentString("nix", "# %s")
 end
-
 
 return M
