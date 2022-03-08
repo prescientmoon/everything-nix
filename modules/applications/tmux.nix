30940be41e9666493e6ccf283dbb12ce81b82789
Matei Adriel <rafaeladriel11@gmail.com>
2022-03-08 12:12:09 +0200

    feat: tmux config in it's own file

--- modules/applications/tmux.nix
+++ modules/applications/tmux.nix
@@ -1,5 +1,27 @@
 { pkgs, ... }: {
-  home-manager.users.adrielus.programs.tmux = {
-    enable = true;
+  home-manager.users.adrielus.programs = {
+    # Add tmux-navigator plugin to neovim
+    neovim.extraPackages = [ pkgs.vimPlugins.vim-tmux-navigator ];
+
+    tmux = {
+      enable = true;
+
+      clock24 = true; # 24h clock format
+      terminal = "screen-256color"; # more colors
+      historyLimit = 10000; # increase amount of saved lines
+
+      plugins = with pkgs.tmuxPlugins; [
+        cpu # Show CPU load with easy icons
+        vim-tmux-navigator # Switch between tmux and vim panes with ease
+      ];
+
+      extraConfig = ''
+        # load the rest of the config
+        source-file ${../../dotfiles/tmux/tmux.conf}
+
+        # Use github light theme
+        source-file ${pkgs.githubNvimTheme}/terminal/tmux/github_light.conf
+      '';
+    };
   };
 }
