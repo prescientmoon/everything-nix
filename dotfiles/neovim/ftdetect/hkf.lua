dab371e809b734f9bde62d7d03b82b11d53635e7
Matei Adriel <rafaeladriel11@gmail.com>
2023-01-10 03:11:44 +0100

    Deleted some useless stuff + fixed a typo

--- dotfiles/neovim/ftdetect/hkf.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  group = vim.api.nvim_create_augroup("Detect hkf", {}),
-  pattern = "*.hkf",
-  callback = function()
-    vim.opt.filetype = "hkf"
-  end
-})
--- scripts/hm-rebuild.sh
+++ scripts/hm-rebuild.sh
@@ -1,4 +1,4 @@
 #!/run/current-system/sw/bin/bash
 home-manager build --flake .#$1
-rm -rf result ~/.local/share/hm-result/$1
+rm -rf ~/.local/share/hm-result/$1
 mv result ~/.local/share/hm-result/$1
