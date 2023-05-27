0503a81ee899dc4d8976636c8ef74e592fe8ad24
Matei Adriel <rafaeladriel11@gmail.com>
2023-05-28 00:41:22 +0200

    Started to rework euporie

--- hosts/nixos/tethys/default.nix
+++ hosts/nixos/tethys/default.nix
@@ -20,34 +20,15 @@
   # Set the name of this machine!
   networking.hostName = "tethys";
 
-  # A few ad-hoc settings
-  hardware.opengl.enable = true;
-  programs.kdeconnect.enable = true;
-  programs.extra-container.enable = true;
-
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "22.11";
 
-  # Temp stuff: enable when running lunarbox
-  # services.postgresql = {
-  #   enable = true;
-  #   package = pkgs.postgresql_15;
-  #   enableTCPIP = true;
-  #   authentication = pkgs.lib.mkOverride 10 ''
-  #     local all all trust
-  #     host all all 127.0.0.1/32 trust
-  #     host all all ::1/128 trust
-  #   '';
-  #   initialScript = pkgs.writeText "backend-initScript" ''
-  #     CREATE ROLE adrielus WITH
-  #            LOGIN
-  #            SUPERUSER
-  #            INHERIT
-  #            CREATEDB
-  #            CREATEROLE
-  #            REPLICATION;
-  #     CREATE DATABASE lunarbox;
-  #     GRANT ALL PRIVILEGES ON DATABASE lunarbox TO adrielus;
-  #   '';
-  # };
+  # {{{ A few ad-hoc hardware settings
+  hardware.opengl.enable = true;
+  hardware.opentabletdrivers.enable = true;
+  # }}}
+  # {{{ A few ad-hoc programs
+  programs.kdeconnect.enable = true;
+  programs.extra-container.enable = true;
+  # }}}
 }
