73a822057ddd9cdec0cb812b503b35b3f98183ee
Matei Adriel <rafaeladriel11@gmail.com>
2023-06-04 08:17:14 +0200

    Fixed bash syntax in vimclip script

--- pkgs/vimclip.nix
+++ pkgs/vimclip.nix
@@ -19,9 +19,9 @@ let vimclip = pkgs.stdenv.mkDerivation rec {
 pkgs.writeShellScriptBin "vimclip" ''
   if ["wayland" = $XDG_SESSION_TYPE]
   then
-    export VIMCLIP_CLIPBOARD_COMMAND = ${pkgs.wl-clipboard}/bin/wl-copy
+    export VIMCLIP_CLIPBOARD_COMMAND=${pkgs.wl-clipboard}/bin/wl-copy
   else
-    export VIMCLIP_CLIPBOARD_COMMAND = ${lib.getExe pkgs.xsel}
+    export VIMCLIP_CLIPBOARD_COMMAND=${lib.getExe pkgs.xsel}
   fi
 
   ${lib.getExe vimclip}
