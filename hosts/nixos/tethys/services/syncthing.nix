cf255939d502db57c32e0a03b04f1ad5c6a81f93
Matei Adriel <rafaeladriel11@gmail.com>
2023-07-18 18:11:07 +0300

    Fixed big syncthing oopsie

--- hosts/nixos/tethys/services/syncthing.nix
+++ hosts/nixos/tethys/services/syncthing.nix
@@ -1,21 +1,17 @@
 {
   imports = [ ../../common/optional/syncthing.nix ];
 
-  services.syncthing = {
-    devices.tethys.id = "NGHX5G4-IY3ZXL2-NQMMRQV-2GDQLC6-LIDWSNG-DAJUAQH-KBAP64V-55K2LQ6";
+  services.syncthing.folders = {
+    "stellar-sanctum" = {
+      path = "/home/adrielus/Projects/stellar-sanctum/";
+      devices = [ "enceladus" "lapetus" ];
 
-    folders = {
-      "stellar-sanctum" = {
-        path = "/home/adrielus/Projects/stellar-sanctum/";
-        devices = [ "enceladus" "lapetus" ];
-
-        # TODO: remove this once I switch to zfs
-        versioning = {
-          type = "staggered";
-          params = {
-            cleanInterval = "3600"; # 1 hour in seconds
-            maxAge = "604800"; # 14 days in seconds.
-          };
+      # TODO: remove this once I switch to zfs
+      versioning = {
+        type = "staggered";
+        params = {
+          cleanInterval = "3600"; # 1 hour in seconds
+          maxAge = "604800"; # 14 days in seconds.
         };
       };
     };
