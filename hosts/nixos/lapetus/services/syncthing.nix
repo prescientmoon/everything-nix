4922fc25424e322492417d2c07a972b567c9d534
Matei Adriel <rafaeladriel11@gmail.com>
2023-07-18 18:06:02 +0300

    Lapetus syncthing device id

--- hosts/nixos/lapetus/services/syncthing.nix
+++ hosts/nixos/lapetus/services/syncthing.nix
@@ -4,7 +4,7 @@
   networking.firewall.allowedTCPPorts = [ 8384 ];
 
   services.syncthing = {
-    devices.lapetus.id = "";
+    devices.lapetus.id = "VVHM7RC-ZSDOZJI-EGBIJR4-2DOGAXG-OEJZWSH-OYUK5XT-7CDMWSL-3AVM2AZ";
     guiAddress = "0.0.0.0:8384"; # TODO: put this behind nginx
 
     folders = { };
