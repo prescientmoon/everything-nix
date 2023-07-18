3d47d56ce47d22159f75e9f619687050681ce211
Matei Adriel <rafaeladriel11@gmail.com>
2023-07-18 18:04:12 +0300

    Open firewall port for syncthing

--- hosts/nixos/lapetus/services/syncthing.nix
+++ hosts/nixos/lapetus/services/syncthing.nix
@@ -1,6 +1,8 @@
 {
   imports = [ ../../common/optional/syncthing.nix ];
 
+  networking.firewall.allowedTCPPorts = [ 8384 ];
+
   services.syncthing = {
     devices.lapetus.id = "";
     guiAddress = "0.0.0.0:8384"; # TODO: put this behind nginx
