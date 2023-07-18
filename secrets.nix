24ce17adec26373b0c691eade4b3fb656ec170ba
Matei Adriel <rafaeladriel11@gmail.com>
2023-07-18 15:31:02 +0300

    Rekeyed secrets to include laleptus (also removed spotifyd)

--- secrets.nix
+++ secrets.nix
@@ -1,13 +1,15 @@
 let
-  adrielus = builtins.readFile ./hosts/nixos/tethys/id_ed25519.pub;
+  adrielus_tethys = builtins.readFile ./hosts/nixos/tethys/id_ed25519.pub;
   tethys = builtins.readFile ./hosts/nixos/tethys/ssh_host_ed25519_key.pub;
+  lapetus = builtins.readFile ./hosts/nixos/lapetus/ssh_host_ed25519_key.pub;
+  all_hosts = [ tethys lapetus ];
 in
 {
   # Scoped for entire systems
-  "./hosts/nixos/common/global/wireless/wifi_passwords.age".publicKeys = [ adrielus tethys ];
-  "./hosts/nixos/common/users/adrielus_password.age".publicKeys = [ adrielus tethys ];
+  "./hosts/nixos/common/global/wireless/wifi_passwords.age".publicKeys = all_hosts ++ [ adrielus_tethys ];
+  "./hosts/nixos/common/users/adrielus_password.age".publicKeys = all_hosts ++ [ adrielus_tethys ];
 
   # Scoped for the user
-  "./home/features/desktop/wakatime/wakatime_config.age".publicKeys = [ adrielus ];
-  "./home/features/desktop/spotifyd/password.age".publicKeys = [ adrielus ];
+  # TODO: move this into `pass`.
+  "./home/features/desktop/wakatime/wakatime_config.age".publicKeys = [ adrielus_tethys ];
 }
