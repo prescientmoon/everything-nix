cb6ad4da32d75b22ee6acdbbdfff18560579aa36
Matei Adriel <rafaeladriel11@gmail.com>
2023-08-29 11:58:32 +0300

    Migrate from leap.nvim to flash.nvim

---
+++ dotfiles/neovim/lua/my/plugins/flash.lua
@@ -0,0 +1,29 @@
+local function keybind(keys, action, desc, modes)
+  if modes == nil then
+    modes = { "n", "x", "o" }
+  end
+
+  return {
+    keys,
+    mode = modes,
+    function()
+      require("flash")[action]()
+    end,
+    desc = desc,
+  }
+end
+
+local M = {
+  "folke/flash.nvim",
+  ---@type Flash.Config
+  opts = {},
+  keys = {
+    keybind("s", "jump", "Flash"),
+    keybind("S", "treesitter", "Flash Treesitter"),
+    keybind("r", "remote", "Remote Flash", { "o" }),
+    keybind("R", "treesitter_search", "Treesitter Search", { "o", "x" }),
+    keybind("<C-S>", "toggle", "Toggle Flash Search", { "c" }),
+  },
+}
+
+return M
