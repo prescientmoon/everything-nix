b8a120b986b144c6e304e357a779c4314ea50aae
Matei Adriel <rafaeladriel11@gmail.com>
2024-01-31 22:09:04 +0100

    Re-enable `pounce` stuff

--- hosts/nixos/lapetus/services/pounce.nix
+++ hosts/nixos/lapetus/services/pounce.nix
@@ -11,16 +11,16 @@ let makeNetworkConfig = host: port: join: secret: {
 in
 {
   security.acme.certs."wildcard-irc.moonythm.dev" = {
-    group = "pounce";
+    group = config.services.pounce.user;
     domain = "*.irc.moonythm.dev";
   };
 
   sops.secrets.tilde_irc_pass.sopsFile = ../secrets.yaml;
   sops.templates."pounce-tilde.cfg" = makeNetworkConfig "eu.tilde.chat" 6697 "#meta" "tilde_irc_pass";
-  # services.pounce = {
-  #   enable = true;
-  #   externalHost = "irc.moonythm.dev";
-  #   certDir = "/var/lib/acme/wildcard-irc.moonythm.dev";
-  #   networks.tilde.config = config.sops.templates."pounce-tilde.cfg".path;
-  # };
+  services.pounce = {
+    enable = true;
+    externalHost = "irc.moonythm.dev";
+    certDir = "/var/lib/acme/wildcard-irc.moonythm.dev";
+    networks.tilde.config = config.sops.templates."pounce-tilde.cfg".path;
+  };
 }
