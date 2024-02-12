27eb310cce02e9933e79520c39a62fc7f9490cda
Matei Adriel <rafaeladriel11@gmail.com>
2024-02-12 19:33:31 +0100

    Fix uptimes script parsing

--- home/features/cli/scripts/uptimes.sh
+++ home/features/cli/scripts/uptimes.sh
@@ -3,7 +3,7 @@ print_uptime() {
   local EMOJI=$2
 
   # -n: do not echo a newline
-  echo -n "$EMOJI $HOST: "
+  echo -n "$EMOJI $HOST: ~"
 
   # The output of `uptime` looks like this:
   # `18:40:54  up 1 day  0:51,  0 users,  load average: 0.79, 0.68, 0.69`,
@@ -12,10 +12,16 @@ print_uptime() {
   # For awk:
   # -F: splits the input by a string
   ssh adrielus@$HOST uptime \
-    | awk -F "up" '{print $2}' \
-    | awk -F " " '{print $1, $2}'
+    | awk -F '(up |,)' '{print $2}'
 }
 
 echo "Uptimes:"
-print_uptime "tethys" "🔥"
-print_uptime "lapetus" "⛵"
+
+{
+  print_uptime "tethys" "🔥"
+  print_uptime "lapetus" "⛵"
+} |
+column --table -R 2 -s "~"
+# ^ We use the column command to align things nicely:
+# -R 2 aligns the second column to the right
+# -s ~ will split on occurences of ~
