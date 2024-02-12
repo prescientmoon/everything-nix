15441e4b611a6ddbec794d60b3832ade57b5f644
Matei Adriel <rafaeladriel11@gmail.com>
2024-02-12 15:38:46 +0100

    Fix msmtp config

--- hosts/nixos/lapetus/services/msmtp.nix
+++ hosts/nixos/lapetus/services/msmtp.nix
@@ -2,13 +2,12 @@
   programs.msmtp = {
     enable = true;
     setSendmail = true;
-    accounts = {
-      defaults = {
-        auth = true;
-        tls = true;
-        host = "smtp.migadu.com";
-        port = 465;
-      };
+
+    defaults = {
+      auth = true;
+      tls = true;
+      host = "smtp.migadu.com";
+      port = 465;
     };
   };
 }
