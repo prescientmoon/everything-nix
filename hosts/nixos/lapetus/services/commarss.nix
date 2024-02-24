444c78be4e01aebe8816252b3c99b403f5b3e1ec
prescientmoon <git@moonythm.dev>
2024-02-24 16:03:18 +0100

    Assign random user agent to commarss

--- hosts/nixos/lapetus/services/commarss.nix
+++ hosts/nixos/lapetus/services/commarss.nix
@@ -21,7 +21,10 @@ in
     # https://github.com/Athou/commafeed/blob/master/commafeed-server/config.yml.example
     environment = {
       CF_APP_PUBLICURL = "https://${host}";
-      CF_APP_ALLOWREGISTRATIONS = "true";
+      CF_APP_ALLOWREGISTRATIONS = "false"; # I already made an account
+
+      # I randomly generated an user agent for this
+      CF_APP_USERAGENT = "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0_6; like Mac OS X) AppleWebKit/533.48 (KHTML, like Gecko)  Chrome/49.0.2557.162 Mobile Safari/602.0";
     };
   };
 }
