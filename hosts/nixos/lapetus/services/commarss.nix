eb7532621f14104debdaab40e1374547c9a4da81
prescientmoon <git@moonythm.dev>
2024-02-25 03:05:35 +0100

    Commafeed get old entires

--- hosts/nixos/lapetus/services/commarss.nix
+++ hosts/nixos/lapetus/services/commarss.nix
@@ -22,6 +22,7 @@ in
     environment = {
       CF_APP_PUBLICURL = "https://${host}";
       CF_APP_ALLOWREGISTRATIONS = "false"; # I already made an account
+      CF_APP_MAXENTRIESAGEDAYS = "0"; # Fetch old entries
 
       # I randomly generated an user agent for this
       CF_APP_USERAGENT = "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0_6; like Mac OS X) AppleWebKit/533.48 (KHTML, like Gecko)  Chrome/49.0.2557.162 Mobile Safari/602.0";
