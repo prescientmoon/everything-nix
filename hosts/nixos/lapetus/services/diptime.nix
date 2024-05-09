256d7bcb4b65915c3b0a1730bd9f4992a71a77a4
prescientmoon <git@moonythm.dev>
2024-05-09 04:39:51 +0200

    Enable cloudflare tunnels

--- hosts/nixos/lapetus/services/diptime.nix
+++ hosts/nixos/lapetus/services/diptime.nix
@@ -17,6 +17,7 @@
   };
 
   services.cloudflared = {
+    enable = true;
     tunnels."347d9ead-a523-4f8b-bca7-3066e31e2952" = {
       credentialsFile = config.sops.secrets.cloudflare_tunnel_credentials.path;
       ingress."diptime.moonythm.dev".service = "http://localhost:80";
