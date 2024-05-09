ec0250ad36198a2099b168ba7356faaba0bdb052
prescientmoon <git@moonythm.dev>
2024-05-09 04:31:39 +0200

    Attempt to set up cloudflare tunnels

--- hosts/nixos/lapetus/services/diptime.nix
+++ hosts/nixos/lapetus/services/diptime.nix
@@ -9,4 +9,17 @@
       rev = "d6ea7b9d9e94ee6d2db8e4e7cff5f8f1c3f04464";
       sha256 = "09s6awz5m6hzpc6jp96c118i372430c7b41acm5m62bllcvrj9vk";
     });
+
+  sops.secrets.cloudflare_tunnel_credentials = {
+    sopsFile = ../secrets.yaml;
+    owner = config.services.cloudflared.user;
+    group = config.services.cloudflared.group;
+  };
+
+  services.cloudflared = {
+    tunnels."347d9ead-a523-4f8b-bca7-3066e31e2952" = {
+      credentialsFile = config.sops.secrets.cloudflare_tunnel_credentials.path;
+      ingress."diptime.moonythm.dev".service = "http://localhost:80";
+    };
+  };
 }
