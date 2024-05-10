bcd6ba0cc41d8ec2d0ab88d9fd8aab424b8293af
prescientmoon <git@moonythm.dev>
2024-05-10 20:23:50 +0200

    Fix microbin uploader pass

--- hosts/nixos/lapetus/services/microbin.nix
+++ hosts/nixos/lapetus/services/microbin.nix
@@ -24,18 +24,23 @@ in
       MICROBIN_ADMIN_USERNAME = "prescientmoon";
       MICROBIN_PORT = toString port;
       MICROBIN_PUBLIC_PATH = "https://bin.moonythm.dev/";
+      MICROBIN_DEFAULT_EXPIRY = "1week";
+
+      # Disable online features
       MICROBIN_DISABLE_TELEMETRY = "true";
       MICROBIN_DISABLE_UPDATE_CHECKING = "true";
 
-      # Toggle certain features
-      MICROBIN_READONLY = "true"; # Requires a password to upload
-      MICROBIN_QR = "true"; # Allows generating qr codes
-      MICROBIN_EDITABLE = "false";
+      # Enable features
       MICROBIN_HIGHLIGHTSYNTAX = "true";
-      MICROBIN_SHOW_READ_STATS = "false";
+      MICROBIN_QR = "true";
+      MICROBIN_READONLY = "true"; # Requires a password for uploads
+
+      # Disable unwanted features
+      MICROBIN_EDITABLE = "false";
       MICROBIN_ENABLE_BURN_AFTER = "false";
       MICROBIN_ENABLE_READONLY = "false";
       MICROBIN_ETERNAL_PASTA = "false";
+      MICROBIN_SHOW_READ_STATS = "false";
 
       # Make UI more minimal
       MICROBIN_HIDE_FOOTER = "true";
