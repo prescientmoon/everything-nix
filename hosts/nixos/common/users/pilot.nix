ec239297dde75d376b87982683df4f913fe2abca
prescientmoon <git@moonythm.dev>
2024-08-28 00:10:02 +0200

    Fix ssh permissions (take 5)

--- hosts/nixos/common/users/pilot.nix
+++ hosts/nixos/common/users/pilot.nix
@@ -63,8 +63,8 @@
     [
       "d ${root} 0755 ${user.name} ${user.group}"
       "d ${root}/.ssh 0755 ${user.name} ${user.group}"
-      "e ${root}/.ssh/id_rsa 0700 ${user.name} ${user.group}"
-      "e ${root}/.ssh/id_ed25519 0700 ${user.name} ${user.group}"
+      "z ${root}/.ssh/id_rsa 0700 ${user.name} ${user.group}"
+      "z ${root}/.ssh/id_ed25519 0700 ${user.name} ${user.group}"
     ];
   # }}}
 }
