52d0513ab38b5b95f7dc37ca2f468e7f328fa318
prescientmoon <git@moonythm.dev>
2024-10-11 12:44:13 +0200

    Fix lapetus invidous db user

--- hosts/nixos/lapetus/services/invidious.nix
+++ hosts/nixos/lapetus/services/invidious.nix
@@ -1,4 +1,5 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+{
   sops.secrets.invidious_hmac_key.sopsFile = ../secrets.yaml;
   sops.templates."invidious_hmac_key.json" = {
     content = ''{ "hmac_key": "${config.sops.placeholder.invidious_hmac_key}" }'';
@@ -18,21 +19,18 @@
       admins = [ "prescientmoon" ];
       default_user_preferences = {
         default_home = "Subscriptions";
-        comments = [ "youtube" "reddit" ];
+        comments = [
+          "youtube"
+          "reddit"
+        ];
         save_player_pos = true;
         automatic_instance_redirect = true;
       };
+
+      # The error when updating to 24.05 asked me to set this
+      db.user = "invidious";
     };
 
-    # REASON: the current invidious is broken, and cannot play videos
-    package = pkgs.invidious.overrideAttrs (_oldAttrs: {
-      src = pkgs.fetchFromGitHub {
-        owner = "iv-org";
-        repo = "invidious";
-        fetchSubmodules = true;
-        rev = "eda7444ca46dbc3941205316baba8030fe0b2989";
-        sha256 = "0iafxgb93jxx9ams6ll2yx8il4d7h89a630hcx9y8jj4gn3ax7v1";
-      };
-    });
+    package = pkgs.invidious;
   };
 }
