f34a19e67eff41645109dd9d001299605a58d775
prescientmoon <git@moonythm.dev>
2025-06-06 10:39:25 +0200

    Add latte nixos wallpaper

--- common/themes/default.nix
+++ common/themes/default.nix
@@ -24,7 +24,7 @@ let
     # {{{ Catppuccin latte
     catppuccin-latte = {
       stylix = {
-        image = ./wallpapers/needygirloverdose.jpg;
+        image = ./wallpapers/nix-catppuccin-lattle.png;
         base16Scheme = base16 "catppuccin-latte";
         opacity = transparency 0.7;
         polarity = "light";
