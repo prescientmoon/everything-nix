bf71d5949ef52bc420cf26f504a7ec6d22114932
prescientmoon <git@moonythm.dev>
2026-02-22 12:15:25 +0100

    Update some thingies for lapetus

--- hosts/nixos/lapetus/services/jellyfin.nix
+++ hosts/nixos/lapetus/services/jellyfin.nix
@@ -1,19 +1,23 @@
-{ config, pkgs, ... }: {
+{ config, ... }:
+{
   # This is the default port, and can only be changed via the GUI
   satellite.nginx.at.media.port = 8096;
   services.jellyfin.enable = true;
 
-  # {{{ Storage 
-  environment.persistence."/persist/state".directories = [{
-    inherit (config.services.jellyfin) user group;
-    directory = "/var/lib/jellyfin";
-    mode = "u=rwx,g=r,o=r";
-  }];
+  # Storage
+  environment.persistence."/persist/state".directories = [
+    {
+      inherit (config.services.jellyfin) user group;
+      directory = "/var/lib/jellyfin";
+      mode = "u=rwx,g=r,o=r";
+    }
+  ];
 
-  environment.persistence."/persist/local/cache".directories = [{
-    inherit (config.services.jellyfin) user group;
-    directory = "/var/cache/jellyfin";
-    mode = "u=rwx,g=,o=";
-  }];
-  # }}}
+  environment.persistence."/persist/local/cache".directories = [
+    {
+      inherit (config.services.jellyfin) user group;
+      directory = "/var/cache/jellyfin";
+      mode = "u=rwx,g=,o=";
+    }
+  ];
 }
